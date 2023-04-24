b1d2566aa15226e596cd06d09ad33f474e1014f4
Christin Soots <christin.soots@gmail.com>
2023-04-24 23:42:21 +0300

    uuendus

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require("express");
-const axios = require("axios");
-const { request } = require("http");
-const { response } = require("express");
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (request, response) => {
+app.get('/', (req, res) => {
     let url = 'https://api.themoviedb.org/3/movie/550?api_key=d6786e3599ab20d71b44ad7c375c6d25';
     axios.get(url)
     .then(response => {
         let data = response.data;
         let releaseDate = new Date(data.release_date).getFullYear();
-        let genresToDisplay = "";
+        let genresToDisplay = '';
         data.genres.forEach(genre => {
-            genresToDisplay = genresToDisplay + `${genre.name},`;
-            
+            genresToDisplay = genresToDisplay + `${genre.name}, `;
         });
-        console.log(genresToDisplay)
-        let genresUpdated = genresToDisplay.slice(0, -2) + ".";
+        
+        let genresUpdated = genresToDisplay.slice(0, -2) + '.';
+        
 
         let posterUrl = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
-        moviePoster.src = posterUrl;
-        moviePoster.alt = `${data.title} poster`;
+        // moviePoster.src = posterUrl;
+        // moviePoster.alt = `${data.title} poster`;
+        
 
-        let curentYear = new Date().getFullYear();
-
-        response.render("index", {
+        let currentYear = new Date().getFullYear();
+        
+        res.render("index", {
             dataToRender: data,
             year: currentYear,
             releaseYear: releaseDate,
@@ -40,6 +39,6 @@ app.get("/", (request, response) => {
        
 });
 
-app.listen(process.emv.PORT || 3000, () => {
+app.listen(process.env.PORT|| 3000, () => {
 console.log("Server is running on port 3000.");
 });
